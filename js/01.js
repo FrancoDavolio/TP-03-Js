a6b2279230fe09e15f4a596eb5ddf744ce60b536
Franco Davolio <francodavolio12@gmail.com>
2022-07-25 11:51:57 -0300

    ejercicio01 terminado

--- js/01.js
+++ js/01.js
@@ -1,3 +1,15 @@
 const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-document.write(meses);
+document.write("<ul>")
+document.write("<li>"+meses[0]+"</li>" + "<br>");
+document.write("<li>"+meses[1]+"</li>" + "<br>");
+document.write("<li>"+meses[2]+"</li>" + "<br>");
+document.write("<li>"+meses[3]+"</li>" + "<br>");
+document.write("<li>"+meses[4]+"</li>" + "<br>");
+document.write("<li>"+meses[5]+"</li>" + "<br>");
+document.write("<li>"+meses[6]+"</li>" + "<br>");
+document.write("<li>"+meses[7]+"</li>" + "<br>");
+document.write("<li>"+meses[8]+"</li>" + "<br>");
+document.write("<li>"+meses[9]+"</li>" + "<br>");
+document.write("<li>"+meses[10]+"</li>" + "<br>");
+document.write("<li>"+meses[11]+"</li>" + "<br>");
+document.write("</ul>")
