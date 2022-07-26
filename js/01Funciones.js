b49dfd47f9fbd1665405b67d5a1aeda75f3a9a31
Franco Davolio <francodavolio12@gmail.com>
2022-07-25 22:56:27 -0300

    ejercicio01F terminado

---
+++ js/01Funciones.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt("Ingrese un numero entero"));
+
+const num = function(num1){
+    if(num1 % 2 == 0){
+        console.log("el numero es par")
+    }else{
+        console.log("El numero es impar")
+    }
+}
